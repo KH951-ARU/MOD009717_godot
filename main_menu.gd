efb0a776d5e3f92bc739494c0215ffff9a8b470f
KH951-ARU <KH951@student.aru.ac.uk>
2025-10-04 00:22:31 +0100

    Step 2 the menu is done

---
+++ main_menu.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+var level: int = 1
+
+func _ready() -> void:
+	$"CenterContainer/settings menu/fullscreen".button_pressed = true if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN else false
+	$"CenterContainer/settings menu/main volume slider".value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
+	$"CenterContainer/settings menu/main SFX slider".value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
+	$"CenterContainer/settings menu/main music volume slider".value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music")))
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file(str("res://levels/",level, ".tscn"))
+
+func _on_settings_pressed() -> void:
+	$"CenterContainer/main buttons".visible = false
+	$"CenterContainer/settings menu".visible = true
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	$"CenterContainer/main buttons".visible = true
+	$"CenterContainer/settings menu".visible = false
+
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+
+
+func _on_main_volume_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_main_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), value)
+
+
+func _on_main_music_volume_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), value)

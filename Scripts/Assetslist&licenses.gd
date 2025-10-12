65eeec1829aedc507ce3a875278622ae4de2e4a1
KH951-ARU <KH951@student.aru.ac.uk>
2025-10-12 19:37:06 +0100

    Restart in level select however begin tower defence instead

---
+++ Scripts/Assetslist&licenses.gd
@@ -0,0 +1,23 @@
+extends Node
+
+#https://zerie.itch.io/tiny-rpg-character-asset-pack
+# License:
+
+#You can use this asset for personal and commercial purpose. 
+#You can edit or modify to fit your game.
+#You can't Resell/redistribute this asset.
+
+
+#https://seliel-the-shaper.itch.io/summer-forest
+#... use the asset(s) you bought to make one free or commercial video game.
+#... share the asset with other people working on your video game.
+#... edit my art in any way you see fit (it is actually encouraged).
+#... credit me, though it is not a requirement.
+
+#https://bdragon1727.itch.io/free-smoke-fx-pixel
+#Free 20 Smoke Fx Pixel 64x64
+#License Rights: - Free to use on non-commercial games, please leave comments and reviews that help motivate me.
+# If you will be using on a commercial game, please contribute (any value).
+ #Modify as desired.
+#You cannot do:
+# Resell / redistribute this asset.
--- levels/Test.gd
+++
@@ -1,5 +0,0 @@
-extends Node3D
-
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	get_tree().change_scene_to_file("res://levels/1.tscn")

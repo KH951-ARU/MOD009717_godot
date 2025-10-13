0f510a87147c8c686f33fee1510756c607c0d59b
Harwood <kh951@student.aru.ac.uk>
2025-10-13 15:17:37 +0100

    Assets has been updated

--- Scripts/Assetslist&licenses.gd
+++ Scripts/Assetslist&licenses.gd
@@ -21,3 +21,19 @@ extends Node
  #Modify as desired.
 #You cannot do:
 # Resell / redistribute this asset.
+
+
+#https://oco.itch.io/medieval-fantasy-character-pack-7
+#https://oco.itch.io/medieval-fantasy-character-pack-4
+#Notes:
+
+#Feel free to use and modify the characters depending on your current project. 
+#If you have any suggestions regarding other characters, items or backgrounds, which can fit with this pack, 
+#please let me know. In case you want to offer me credits, please use my current username. 
+#Also, keep in mind that you can use this in your commercial projects, 
+#but please do not resell the assets individually. 
+
+#https://luizmelo.itch.io/monsters-creatures-fantasy
+#This package can be used in commercial and non-commercial projects - CC0(creative commons zero).
+
+#Credits are not required but I would appreciate it.

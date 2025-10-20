e000ab91bacdc835a614f9fdfc41d50d5fad5395
Harwood <kh951@student.aru.ac.uk>
2025-10-20 15:11:41 +0100

    Step 7 The bones are shaking

--- Scripts/Enermies/skeleton.gd
+++ Scripts/Enermies/skeleton.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
-@export var speed : float = 100
+@export var speed : float = 0.01
 @export var positioncache : Vector2 
 @export var spriteholder : AnimatedSprite2D
+@export var pathfollower : PathFollow2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready() -> void:
 	spriteholder.play("Walk")
 	
 func _process(delta: float) -> void:
-	get_parent().set_process(get_parent().get_progress() + speed * delta)
-	
+	pathfollower.progress_ratio += (speed * delta)
+	if pathfollower.progress_ratio == 1:
+		queue_free()

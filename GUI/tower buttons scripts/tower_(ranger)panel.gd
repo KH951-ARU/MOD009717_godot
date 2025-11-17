8498f6b188e858be0abef846b0238756a8596ca5
Harwood <kh951@student.aru.ac.uk>
2025-11-17 15:52:50 +0000

    Step 18 upgrades are begin development

--- GUI/tower buttons scripts/tower_(ranger)panel.gd
+++ GUI/tower buttons scripts/tower_(ranger)panel.gd
@@ -2,36 +2,46 @@ extends Panel
 @export var tower1 :PackedScene
 var currentTile
 var tempTower
+var onscreen : bool = false
+var PathMap
+var Isotile
 func _on_gui_input(event):
-	
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT: #left click down
-		var tempTower = tower1.instantiate()
+
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed: #left click down
+		tempTower = tower1.instantiate()
 		
 		print("left button down")
 		add_child(tempTower)
-		
+
 		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
-		tempTower.scale = Vector2(0.32,0.32) # scale for the tower
-		
-	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT: #drag left click
-		get_child(1).global_position = event.global_position
-		print("dragging")
-		
+		tempTower.scale = Vector2(0.75,0.75) # scale for the tower
+
+	elif event is InputEventMouseMotion:
+		if tempTower and is_instance_valid(tempTower):
+			tempTower.global_position = event.global_position
+			print("dragging")
+			
+			PathMap = get_tree().get_root().find_child("tower objection",true, false) as TileMapLayer
+			Isotile = PathMap.local_to_map(get_global_mouse_position())
+			currentTile = PathMap.get_cell_atlas_coords(Isotile)
+			if (currentTile == Vector2i(11,9)or Vector2i(11,10) or Vector2i(15,8) or Vector2i(15,9) or Vector2i(15,10)):
+				tempTower.get_node("Area").modulate = Color8(49,91,92,255)
+			else:
+				tempTower.get_node("Area").modulate = Color8(255,0,0,145)
+
 	elif  event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed: #left click up
 		print("left button up")
-		
+
 		var dropPOS = get_tree().get_root().get_node("Level 1").get_global_mouse_position() # place location
 		if event.global_position.x >= 2000:
-			if get_child_count() > 1:
-				get_child(1).queue_free()
-				tempTower = tower1.instantiate()
+			tempTower.queue_free()
 		else:
-			if get_child_count() > 1:
-				get_child(1).queue_free()
+			remove_child(tempTower)
 			var Towerpath = get_tree().get_root().get_node("Level 1/Tower")
 			Towerpath.add_child(tempTower)
 			tempTower.global_position = dropPOS
 			tempTower.get_node("Area").hide()
-	else:
-		if get_child_count() > 1:
-			get_child(1).queue_free()
+	#else:
+		#if get_child_count() > 1:
+			#get_child(1).queue_free()
+ 
